2f4b74bdb78480694cf00f7557f6d3afdfa5f6a4
Matthias Paulmier <hello@mattplm.fr>
2022-02-09 09:38:48 +0100

    Properly install .gitconfig on each system

--- install.sh
+++ install.sh
@@ -7,3 +7,13 @@ stow awesome
 
 mkdir -p $HOME/.bin
 ln -t $HOME/.bin bin/*
+
+# Write git config with env variables
+# Needs $NAME and $EMAIL to be set
+
+echo "[commit]
+	template = ~/.gitmessage
+[user]
+	name = $NAME
+	email = $EMAIL
+" >> $HOME/.gitconfig
